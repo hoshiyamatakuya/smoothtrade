08ae918def91a8883497ade5c605a32b9069c8b5
hoshiyamatakuya <andytakuya4166@gmail.com>
2022-12-12 15:12:17 +0900

    post index new _form _post view作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :post_category
+
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :body, presence: true, length: { maximum: 1000 }
 end
