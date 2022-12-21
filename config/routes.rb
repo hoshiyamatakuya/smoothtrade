7ac4358a8b1c86af97d8a7af53a36b2236cc75dc
hoshiyamatakuya <andytakuya4166@gmail.com>
2022-12-21 19:09:15 +0900

    コメント ajax機能　削除作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts do
-    resources :comments, only: %i[create], shallow: true
+    resources :comments, only: %i[create destroy], shallow: true
   end
   devise_for :users
   root 'homes#top'
