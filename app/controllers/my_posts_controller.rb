5b955850dab06ef40aba3163ea541e451e70d2c5
hoshiyamatakuya <andytakuya4166@gmail.com>
2022-12-21 23:09:03 +0900

    my_posts#index ログイン中ユーザーの出品一覧

---
+++ app/controllers/my_posts_controller.rb
@@ -0,0 +1,6 @@
+class MyPostsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @posts = Post.all.includes(:user).order(created_at: :desc)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'my_posts/index'
   resources :posts do
     resources :comments, only: %i[create destroy], shallow: true
   end
