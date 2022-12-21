a1e95d1a213caaa4a4f3a55f26c5860d3410f52d
hoshiyamatakuya <andytakuya4166@gmail.com>
2022-12-21 14:02:30 +0900

    edit update delete 実装ok post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+
+  def own?(object)
+    id == object.user_id
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
