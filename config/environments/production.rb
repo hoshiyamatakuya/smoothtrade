13977c339e54f272b22beff6f95ff2ba92554c8c
hoshiyamatakuya <andytakuya4166@gmail.com>
2023-02-04 16:26:57 +0900

    port,25 productionに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'smoothtrade.magia.runteq.jp' }
+  config.action_mailer.default_url_options = { host: 'smoothtrade.magia.runteq.jp', port: 25 }
 end
