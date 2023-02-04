7978829ea53593f36465369f2dcc0e8ce85b2d57
hoshiyamatakuya <andytakuya4166@gmail.com>
2023-02-04 16:25:12 +0900

    port,25に変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,13 +60,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 25 }
   # mail setting
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
+    :domain => 'gmail.com',
     :user_name => Settings.gmail[:user_name],
     :password => Settings.gmail[:password],
     :authentication => :plain,
