2c8762ea4a0d2d9b71f3b37efbe1165cbfaa7c0e
hoshiyamatakuya <andytakuya4166@gmail.com>
2023-02-04 16:58:01 +0900

    production maiolar設定全てペースト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 25 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # mail setting
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
