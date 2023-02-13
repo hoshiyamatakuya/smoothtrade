5bca53ff39efd6dfef2f7aac2363b13a2f44cb80
hoshiyamatakuya <andytakuya4166@gmail.com>
2023-02-13 15:16:24 +0900

    entrypoint編集

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
+rake db:migrate:reset
 bin/rails db:migrate
-#bin/rails db:seed
+bin/rails db:seed
 bin/rails sitemap:refresh
 whenever --update-crontab
 bin/rails s
