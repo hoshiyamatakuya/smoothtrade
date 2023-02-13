285240eb40d50d402dadb8f1b8be588c27e28bb2
hoshiyamatakuya <andytakuya4166@gmail.com>
2023-02-13 16:44:24 +0900

    entrypoint編集3

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
-run rails db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+#rails db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bin/rails db:migrate
-bin/rails db:seed
+#bin/rails db:seed
 bin/rails sitemap:refresh
 whenever --update-crontab
 bin/rails s
