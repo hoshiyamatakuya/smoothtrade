506c3212624ae70cd0227ae652960543d1627cb5
hoshiyamatakuya <andytakuya4166@gmail.com>
2023-02-13 16:27:38 +0900

    entrypoint編集

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
-rails db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+#rails db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bin/rails db:migrate
 bin/rails db:seed
 bin/rails sitemap:refresh
