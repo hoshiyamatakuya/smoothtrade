0bd6bba9daa3fd626e31c77853640eb26db2f974
hoshiyamatakuya <andytakuya4166@gmail.com>
2023-02-14 18:49:54 +0900

    entrypoint編集6

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://smoothtrade.magia.runteq.jp"
+SitemapGenerator::Sitemap.default_host = "https://smoothtrade.link"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 #rails db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bin/rails db:migrate
 bin/rails db:seed
-#bin/rails sitemap:refresh
-#whenever --update-crontab
+bin/rails sitemap:refresh
+bundle exec whenever --update-crontab
 bin/rails s
